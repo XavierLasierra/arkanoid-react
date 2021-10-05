0ac49aff06e844227ffc4d62b7a4333e9f96516f
Xavier Lasierra <xlp1998@gmail.com>
2021-10-05 12:01:11 +0200

    Create GameInformation component and add style to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import GamePage from './pages/GamePage/GamePage';
+import GamePage from './components/GamePage/GamePage';
 
 import './app.styles.scss';
+import GameInformation from './components/GameInformation/GameInformation';
 
 function App() {
   return (
     <main className="main">
       <GamePage />
+      <GameInformation />
     </main>
   );
 }
---
+++ src/components/GameInformation/GameInformation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './gameInformation.styles.scss';
+
+export default function GameInformation() {
+  return (
+    <section className="game-information">
+      <div className="web-title">
+        <h1 className="web-title__main-title">Arkanoid</h1>
+        <h2 className="web-title__subtitle">by Xavier Lasierra</h2>
+      </div>
+      <p className="game-information__description">
+        This application is a simple version of the arcade game
+        {' '}
+        <a className="game-information__game-link" href="https://en.wikipedia.org/wiki/Arkanoid">arcanoid</a>
+        .
+      </p>
+    </section>
+  );
+}
