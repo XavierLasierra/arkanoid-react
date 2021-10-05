e366d861c322a71a1adc14284ce44aa44308e6d7
Xavier Lasierra <xlp1998@gmail.com>
2021-10-05 13:06:50 +0200

    Create EditingInformation and GameInformation components

--- src/components/GameInformation/GameInformation.tsx
+++ src/components/GameInformation/GameInformation.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { canPlayState } from '../../redux/actions/gameState.creator';
+import EditingInformation from '../EditingInformation/EditingInformation';
+import PlayingInformation from '../PlayingInformation/PlayingInformation';
 
 import './gameInformation.styles.scss';
 
 export default function GameInformation() {
+  const { canPlay, canEdit } = useSelector((store: any) => store.gameState);
   const dispatch = useDispatch();
   function handleCanPlay() {
     dispatch(canPlayState());
   }
 
+  const userInteractive = canPlay
+    ? <PlayingInformation />
+    : <EditingInformation />;
+
   return (
-    <section className="game-information">
-      <div className="web-title">
-        <h1 className="web-title__main-title">Arkanoid</h1>
-        <h2 className="web-title__subtitle">by Xavier Lasierra</h2>
-      </div>
-      <p className="game-information__description">
-        This application is a simple version of the arcade game
-        {' '}
-        <a className="game-information__game-link" href="https://en.wikipedia.org/wiki/Arkanoid">arcanoid</a>
-        .
-      </p>
-      <div className="game-information__buttons">
-        <button className="button" type="button" onClick={handleCanPlay}>START</button>
-        <button className="button" type="button">EDIT</button>
-      </div>
-    </section>
+    <div className="information-container">
+      {(canPlay || canEdit)
+        ? userInteractive
+        : (
+          <section className="game-information">
+            <div className="web-title">
+              <h1 className="web-title__main-title">Arkanoid</h1>
+              <h2 className="web-title__subtitle">by Xavier Lasierra</h2>
+            </div>
+            <p className="game-information__description">
+              This application is a simple version of the arcade game
+              {' '}
+              <a className="game-information__game-link" href="https://en.wikipedia.org/wiki/Arkanoid">arcanoid</a>
+              .
+            </p>
+            <div className="game-information__buttons">
+              <button className="button" type="button" onClick={handleCanPlay}>START</button>
+              <button className="button" type="button">EDIT</button>
+            </div>
+          </section>
+        )}
+    </div>
   );
 }
