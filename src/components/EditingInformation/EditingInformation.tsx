cc54f5b5430937c7efa0233957b27acae31b390b
Xavier Lasierra <xlp1998@gmail.com>
2021-10-12 15:09:19 +0200

    Delete unnecesary import in editinInformation scss

--- src/components/EditingInformation/EditingInformation.tsx
+++ src/components/EditingInformation/EditingInformation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+
 import { discardBoardChanges, startBoardSave } from '../../redux/actions/gameState.creator';
 
 import './editingInformation.styles.scss';
