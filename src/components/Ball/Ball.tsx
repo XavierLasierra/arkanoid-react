0eb600172c5361c976041abc4d31413ef09b0687
Xavier Lasierra <xlp1998@gmail.com>
2021-10-12 14:48:00 +0200

    Move Ball styles to ball.styles.scss

--- src/components/Ball/Ball.tsx
+++ src/components/Ball/Ball.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { IBallProps } from '../../types/interfaces';
+
 import {
   ballSize, dohGameBoard, gameBoardSize,
 } from '../../constants/gameBoard.constants';
-import { IBallProps } from '../../types/interfaces';
 
 import './ball.styles.scss';
 
@@ -10,13 +11,11 @@ export default function Ball({ dohCoordinateX, ballCoordinates, isGameActive }: 
   const ballStyles = {
     width: ballSize.width,
     height: ballSize.heigth,
-    borderRadius: '50%',
-    backgroundColor: '#00F',
-    boxShadow: '0 0 20px #0CF',
     transform: isGameActive ? `translate(${(ballSize.width / 2) + (ballCoordinates[0] / dohGameBoard.length) * gameBoardSize.width}px, ${-gameBoardSize.height + (ballCoordinates[1] / dohGameBoard.length) * gameBoardSize.height}px)` : `translate(${dohCoordinateX - ballSize.width / 2}px, ${-ballSize.heigth}px)`,
     transition: isGameActive ? 'transform 0.1s linear' : 'none',
   };
+
   return (
-    <div style={ballStyles} className="ball" />
+    <div className="ball" style={ballStyles} />
   );
 }
